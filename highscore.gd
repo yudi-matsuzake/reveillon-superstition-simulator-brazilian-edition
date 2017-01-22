773d22fb2c6915af02ea16a36e83003820323a26
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2017-01-22 15:07:03 -0200

    Add highscore module

---
+++ highscore.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var file_manager = File.new()
+var save_path = "user://hightscore.save"
+var highscore_data = {
+				"highscore" : {
+					"name" : "",
+					"highscore" : 0
+				}}
+
+func _ready():
+	set_highscore("yudi", 0)
+
+func set_highscore(name, highscore):    
+	highscore_data["name"] = name
+	highscore_data["highscore"] = highscore #data to save
+	file_manager.open(save_path, File.WRITE) #open file to write
+	file_manager.store_var(highscore_data) #store the data
+	file_manager.close() # close the file
+
+func get_highscore():
+	if not file_manager.file_exists(save_path):
+		return null
+	file_manager.open(save_path, File.READ) #open the file
+	var save_data = file_manager.get_var() #get the value
+	file_manager.close() #close the file
+
+	return save_data
+
+func check_and_save_highscore(name, highscore):
+	var stored_highscore = get_highscore()
+	if ( stored_highscore != null and
+		 int(stored_highscore["highscore"]) < highscore):
+		set_highscore(name, highscore)
