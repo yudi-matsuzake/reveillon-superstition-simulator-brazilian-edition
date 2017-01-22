d20648e66f9f3a99f917eb85ffa173e7b9bedba3
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2017-01-22 16:41:05 -0200

    REAL fix the highscore (i guess)

--- highscore.gd
+++ highscore.gd
@@ -3,17 +3,14 @@ extends Node2D
 var file_manager = File.new()
 var save_path = "user://highscore.save"
 var highscore_data = {
-				"highscore" : {
 					"name" : "",
 					"highscore" : 0
-				}}
+					}
 
 func _ready():
 	pass
 
 func set_highscore(name, highscore):
-	print('name ' + str(name))
-	print('highscore ' + str(highscore))
 	highscore_data["name"] = name
 	highscore_data["highscore"] = highscore #data to save
 	file_manager.open(save_path, File.WRITE) #open file to write
@@ -22,15 +19,21 @@ func set_highscore(name, highscore):
 
 func get_highscore():
 	if not file_manager.file_exists(save_path):
-		return null
+		create_save()
+		return highscore_data
 	file_manager.open(save_path, File.READ) #open the file
 	var save_data = file_manager.get_var() #get the value
 	file_manager.close() #close the file
 
 	return save_data
 
+func create_save():
+   file_manager.open(save_path, File.WRITE)
+   file_manager.store_var(highscore_data)
+   file_manager.close()
+
 func check_and_save_highscore(name, highscore):
 	var stored_highscore = get_highscore()
-	if ( stored_highscore != null and
-		 int(stored_highscore["highscore"]) < highscore):
+	
+	if ( int(stored_highscore["highscore"]) < highscore):
 		set_highscore(name, highscore)
